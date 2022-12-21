964f210a07e65c22588eaa960d0ac73723c3f40f
Taiwo Akerele <taiwoakerele98@gmail.com>
2022-12-21 22:25:39 +0100

    python classes

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-# Define a class Square.
+"""Define a class Square."""
 
 
 class Square:
-    # Represent a square.
+    """Represent a square."""
 
     def __init__(self, size=0):
         """Initialize a new square.
@@ -18,5 +18,5 @@ class Square:
         self.__size = size
 
     def area(self):
-        # Return the current area of the square.
+        """Return the current area of the square."""
         return (self.__size * self.__size)
