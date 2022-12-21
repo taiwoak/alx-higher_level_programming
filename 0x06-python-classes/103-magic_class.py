df0ac9678e562f5259bebc9a75771d88b7af8f38
Taiwo Akerele <taiwoakerele98@gmail.com>
2022-12-21 22:24:18 +0100

    python classes

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-# Define a MagicClass matching exactly a bytecode provided by Holberton.
+"""Define a MagicClass matching exactly a bytecode provided by Holberton."""
 
 import math
 
 
 class MagicClass:
-    # Represent a circle.
+    """Represent a circle."""
 
     def __init__(self, radius=0):
         """Initialize a MagicClass.
@@ -19,9 +19,9 @@ class MagicClass:
         self.__radius = radius
 
     def area(self):
-        # Return the area of the MagicClass.
+        """Return the area of the MagicClass."""
         return (self.__radius ** 2 * math.pi)
 
     def circumference(self):
-        # Return The circumference of the MagicClass.
+        """Return The circumference of the MagicClass."""
         return (2 * math.pi * self.__radius)
