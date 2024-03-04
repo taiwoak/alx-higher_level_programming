44bbadcf35c34763b52df6b1fee5dbf11b35de69
Taiwo Akerele <taiwoakerele98@gmail.com>
2024-03-04 16:29:30 +0100

    python network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,5 +2,3 @@
 # Make request and display response
 curl -sI "$1" | grep "Content-Length" | cut -d ' ' -f 2
 
-git update-index --chmod=-x alx-higher_level_programming/0x10-python-network_0/0-body_size.sh
-
